e7abc5eb4ac76a34f486852eb86a778eabe77170
Bea <bcampdat@gmail.com>
2024-05-25 22:19:47 +0200

    add code

--- my_file.py
+++ my_file.py
@@ -3,10 +3,11 @@ print('Hi Git')
 print('Hi from another dev')
 
 
-def greeting ():
-    print(f'Hi There')
-
 print ('Hi')
+
+
+def hi_there():
+    print('Hi there')
 
 def my_func():
     print('Hi from my_func')
