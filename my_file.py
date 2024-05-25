c24adcf9169bc8eb48bc8db9605bfe87df9591b4
Bea <bcampdat@gmail.com>
2024-05-25 21:51:44 +0200

    clean

--- my_file.py
+++ my_file.py
@@ -1,14 +1,12 @@
 print('Hi Git')
 
 print('Hi from another dev')
-<<<<<<< HEAD
 
 
-<<<<<<< HEAD
+
+
 print ('Hi')
-=======
+
 def my_func():
     print('Hi from my_func')
->>>>>>> 34127df (add new phyton code)
-=======
->>>>>>> 92fbfb7022693db9e823a62ba73f460e02aba5db
+
